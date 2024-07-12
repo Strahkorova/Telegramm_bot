f4e2783de12a4dc359db6377d991b39bc2112785
d.bayramov <bairamov.dzhamil@mail.ru>
2024-07-12 14:41:04 +0400

    добавил скорость

--- Class_vent.py
+++ Class_vent.py
@@ -3,18 +3,39 @@ import telebot
 bot = telebot.TeleBot('7336100479:AAE_KgTsKoCwMe1rctfOIDtfw0HgOnLzk4E')
 
 but_ventilation = telebot.types.InlineKeyboardMarkup()
-but_ventilation.row(telebot.types.InlineKeyboardButton('Скорость воздуха в сечении', callback_data='vent-1'))
+but_ventilation.row(telebot.types.InlineKeyboardButton('Скорость воздуха в воздуховоде', callback_data='vent-1'))
 but_ventilation.row(telebot.types.InlineKeyboardButton('Площадь воздуховода от скорости', callback_data='vent-2'))
 but_ventilation.row(telebot.types.InlineKeyboardButton('Расход воздуха от скорости и сечения', callback_data='vent-3'))
 
 
+class calculation:
 
-def op(message, D):
-    global L
-    L = message.text
-    try:
-        F = (3.14 * float(D) ** 2) / 4
-        val = float(L) / (float(F) * 3600)
-        bot.send_message(message.chat.id, val)
-    except:
-        bot.send_message(message.chat.id, "ошибка ведите /start")
+    #Расчет скосроти воздуха в воздуховоде
+    def scorost(message, F):
+        global L
+        L = message.text
+        try:
+            val = float(L) / (float(F) * 3600)
+            bot.send_message(message.chat.id, (f'Скорость в воздуховоде равна - {round(val, 1)} м/с'))
+        except:
+            bot.send_message(message.chat.id, "ошибка ведите /start")
+
+    # Расчет площади воздуховода
+    def ploshad(message, L):
+        global v
+        v = message.text
+        try:
+            F = float(L) / (float(v) * 3600)
+            bot.send_message(message.chat.id, (f'Площадь воздуховода равна - {round(F, 5)} м2'))
+        except:
+            bot.send_message(message.chat.id, "ошибка ведите /start")
+
+    #Расчет объекмного расхода воздуха
+    def rashod(message, F):
+        global v
+        v = message.text
+        try:
+            L = (float(F) * (float(v) * 3600))
+            bot.send_message(message.chat.id, (f'Расход воздуха равен - {round(L, 5)} м3/час'))
+        except:
+            bot.send_message(message.chat.id, "ошибка ведите /start")
--- My_bot.py
+++ My_bot.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-
+from Class_vent import calculation
 import Class_vent
 
 bot = telebot.TeleBot('7336100479:AAE_KgTsKoCwMe1rctfOIDtfw0HgOnLzk4E')
@@ -26,22 +26,83 @@ def callback_worker(call):
     if call.data == "vent-1":
         bot.edit_message_text(f'Необходимо выбрать тип воздуховода:\n Круглый - /round \n Прямоугольный - /rectangle',
             call.message.chat.id, call.message.message_id, parse_mode='html')
+    elif call.data == "vent-2":
+        num = bot.send_message(call.message.chat.id, 'Укажите расход воздуха в м3/час')
+        bot.register_next_step_handler(num, scor_1)
+    elif call.data == "vent-3":
+        bot.edit_message_text(f'Необходимо выбрать тип воздуховода:\n Круглый - /round_1 \n Прямоугольный - /rectangle_1',
+            call.message.chat.id, call.message.message_id, parse_mode='html')
+
+#Расчет площади сечения воздуховода
+def scor_1(message):
+    global L
+    L = message.text
+    operu = bot.send_message(message.chat.id, 'Укажите скорость воздуха в м/с')
+    bot.register_next_step_handler(operu, scor_2)
+def scor_2(message):
+    calculation.ploshad(message, L)
 
 
 
+
+
+
+
+
+#Расчет скорости воздуха в круглом воздуховоде
 @bot.message_handler(commands=['round'])
-def callback(message):
-    numtwo_a = bot.send_message(message.chat.id, 'Введите диаметр воздуховода в метрах')
-    bot.register_next_step_handler(numtwo_a, num2_fun)
-
-def num2_fun(message):
+def round_1(message):
+    num = bot.send_message(message.chat.id, 'Введите диаметр воздуховода в мм')
+    bot.register_next_step_handler(num, round_2)
+def round_2(message):
     global D
     D = message.text
     operu = bot.send_message(message.chat.id, 'Укажите расход воздуха в м3/час')
-    bot.register_next_step_handler(operu, operi)
+    bot.register_next_step_handler(operu, round_3)
+def round_3(message):
+    F = (3.14 * (float(D)/1000) ** 2) / 4
+    calculation.scorost(message, F)
 
-def operi(message):
-    Class_vent.op(message, D)
+
+#Расчет скорости воздуха и расход в прямоугольном воздуховоде
+@bot.message_handler(commands=['rectangle', 'rectangle_1'])
+def comm(message):
+    global com
+    com = message.text
+    rectangle_1(message)
+def rectangle_1(message):
+    num = bot.send_message(message.chat.id, 'Введите ширину воздуховода в мм')
+    bot.register_next_step_handler(num, rectangle_2)
+
+def rectangle_2(message):
+    global A
+    A = message.text
+    num_2 = bot.send_message(message.chat.id, 'Введите высоту воздуховода в мм')
+    bot.register_next_step_handler(num_2, rectangle_3)
+
+def rectangle_3(message):
+    global B
+    B = message.text
+    if com == '/rectangle':
+        operu = bot.send_message(message.chat.id, 'Укажите расход воздуха в м3/час')
+        bot.register_next_step_handler(operu, rectangle_4)
+    elif com == '/rectangle_1':
+        operu = bot.send_message(message.chat.id, 'Укажите скорость воздуха в м/с')
+        bot.register_next_step_handler(operu, rectangle_4)
+    else:
+        bot.send_message(message.chat.id, 'Произошла ошибка')
+
+def rectangle_4(message):
+    F = (float(A)*float(B))/1000000
+    if com == '/rectangle':
+        calculation.scorost(message, F)
+    elif com == '/rectangle_1':
+        calculation.rashod(message, F)
+
+
+
+
+
 
 
 
